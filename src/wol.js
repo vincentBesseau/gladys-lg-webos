6f5ca385bda12c55f47bfb22f883ae9d367ce728
vbesseau <vbdev.freelance@gmail.com>
2026-08-09 17:28:32 +0200

    Fix wol

--- src/wol.js
+++ src/wol.js
@@ -2,27 +2,48 @@ import dgram from 'node:dgram';
 
 export function buildMagicPacket(mac) {
   const bytes = mac.split(':').map((part) => Number.parseInt(part, 16));
-  if (bytes.length !== 6 || bytes.some((value) => Number.isNaN(value))) {
+
+  if (bytes.length !== 6 || bytes.some(Number.isNaN)) {
     throw new Error('Invalid MAC address.');
   }
+
   return Buffer.concat([
     Buffer.alloc(6, 0xff),
     ...Array.from({ length: 16 }, () => Buffer.from(bytes)),
   ]);
 }
 
-export async function wakeOnLan(mac, broadcastAddress = '255.255.255.255', port = 9) {
-  const socket = dgram.createSocket('udp4');
-  const packet = buildMagicPacket(mac);
-  try {
-    await new Promise((resolve, reject) => {
-      socket.once('error', reject);
-      socket.bind(() => {
-        socket.setBroadcast(true);
-        socket.send(packet, port, broadcastAddress, (error) => (error ? reject(error) : resolve()));
+function sendPacket(packet, broadcastAddress, port) {
+  return new Promise((resolve, reject) => {
+    const socket = dgram.createSocket('udp4');
+
+    socket.once('error', (error) => {
+      socket.close();
+      reject(error);
+    });
+
+    socket.bind(0, () => {
+      socket.setBroadcast(true);
+
+      socket.send(packet, port, broadcastAddress, (error) => {
+        socket.close();
+
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
       });
     });
-  } finally {
-    socket.close();
-  }
+  });
+}
+
+export async function wakeOnLan(mac, broadcastAddress = '192.168.1.255', port = 9) {
+  const packet = buildMagicPacket(mac);
+
+  await Promise.all([
+    sendPacket(packet, broadcastAddress, port),
+    sendPacket(packet, broadcastAddress, port),
+    sendPacket(packet, broadcastAddress, port),
+  ]);
 }
